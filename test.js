7aff5e17e3cf8ad425511364c79db489161ab783
Diego <Diego@eharvesthub.com>
2016-05-10 11:27:51 -0600

    Changing port

--- test.js
+++ test.js
@@ -5,5 +5,5 @@ server(function(req, res){
 	res.writeHead(200,{'Content-Type': 'text/plain'});
 	res.end('Esto sirve');
 	console.log(req.method + ' ' + req.url);
-}).listen(8080,'127.0.0.1');
+}).listen(8081,'127.0.0.1');
 console.log('El servidor esta corriendo en http://localhost:8080');
