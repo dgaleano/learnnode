18ad3349b39f570a9aadf1d7d60637e6097089e3
Diego <Diego@eharvesthub.com>
2016-05-10 11:21:55 -0600

    Recieving data from the client

--- chat.js
+++ chat.js
@@ -1,10 +1,12 @@
+'use strict';
 let chat = require('net');
 let server = chat.createServer();
 
 server.on('connection',function(client){
 	client.write('hola \n ');
-	client.write('adios \n');
-	client.end();
+	client.on('data',recieve=>{
+		console.log(recieve);
+	});
 });
 server.listen(9000);
 console.log('Chat server runnig');
